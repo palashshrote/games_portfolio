e52ee9ed66e663aed6a1fdf3d0d768d98582b7d6
palashshrote <palash.shrote.58@gmail.com>
2024-02-04 18:33:39 +0530

    created custorm hook for fetching games

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -1,24 +1,9 @@
-import { useEffect, useState } from "react";
-import apiClient from "../Services/api-client";
 import { Text } from "@chakra-ui/react";
+import useGame from "../Hooks/useGame";
 
-interface Game {
-  id: number;
-  name: string;
-}
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
+  const { games, error } = useGame();
+
   return (
     <>
       {error && <Text>{error}</Text>}
